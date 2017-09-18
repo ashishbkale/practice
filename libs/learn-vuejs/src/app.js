c148718a663f3e547de3bdefe377f0549204de50
Srinivas Kasiriveni <kasiriveni@gmail.com>
2017-09-18 19:52:35 +0530

    adding the vue examples

--- libs/learn-vuejs/src/app.js
+++ libs/learn-vuejs/src/app.js
@@ -1,6 +1,38 @@
 var app = new Vue({
     el:"#app",
     data:{
-      message:"learing vue "  
+      message:"learing vue ",
+      msg: "vue is awesome",
+      seen:true ,
+      name:"vuejs",
+      todos:[
+        {
+          text:'Learn JS'
+        },
+        {
+          text:'Learn vue'
+        }
+      ]
+    },
+    methods:{
+      reverseMessage:function(){
+        //console.log(this)
+        this.message = this.message.split('').reverse().join('')
+      }
     }
+});
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+new Vue({
+  el: '#custom-directive',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
 })
