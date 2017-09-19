8eaee11f614abf3adc423ce1eccec766fd90140b
Srinivas Kasiriveni <kasiriveni@gmail.com>
2017-09-19 12:11:36 +0530

    adding the folder

--- frameworks/js/learn-angularjs/src/app.js
+++ frameworks/js/learn-angularjs/src/app.js
@@ -2,4 +2,3 @@ import angular from 'angular';
 import css from './css/style.css';
 import AppModule from './app/app.modules';
 import MainCtrl from './app/controller/main.controller';
-//import {UserService} from './app/services/user.service';
