6d6457d16aff8221fcd15def5186cc9bf62b3fd3
Srinivas Kasiriveni <kasiriveni@gmail.com>
2017-09-22 11:34:16 +0530

    Meta tweaks

--- web/es6/js/app.js
+++ web/es6/js/app.js
@@ -1,2 +1,23 @@
 let x=1;
 console.log(x)
+const person="Nick";
+//person="John";
+console.log(person)
+function myFunction(){
+    var myVar = "srinvias";
+    console.log(myVar)
+}
+
+myFunction()
+console.log(myVar)
+
+const lazyPerson = {
+    name:"srinivas"
+}
+
+console.log(lazyPerson.name)
+lazyPerson.name= "get";
+name="asdfa";
+
+
+
